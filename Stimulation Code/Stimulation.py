1d173862ba6cf71ef8002415c50cabe9f611371c
unknown <zhangqf@shanghaitech.edu.cn>
2018-01-19 16:07:08 +0800

    Stimulation Code for my mode

---
+++ Stimulation Code/Stimulation.py
@@ -0,0 +1,22 @@
+import random
+import sys
+f = open("output.csv","w")
+def Rp(value,num):
+	value_left = value
+	result = []
+	for i in range(num-1):
+		now = random.uniform(0, (2/(num-i))*value_left)
+		now = float('%.2f' %now)
+		result.append(now)
+		value_left = value_left - now
+		value_left = float('%.2f' %value_left)
+	result.append(float('%.2f' %value_left))
+	result_str = ''
+	for i in result:
+		result_str = result_str + '%.2f,'%i
+	result_str = result_str.rstrip(',')
+	print(result_str, file = f)
+'''	
+for j in range(1000000):
+	Rp(10,5)
+'''
